0c7ffd869e49caa5b2050c679d48038392505bc8
Ionut <olaru.ionut01@gmail.com>
2026-03-27 12:56:19 +0200

    Fix lint issues (flake8) and ci.yaml added

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route("/health") 
+
+@app.route("/health")
 def health():
     return jsonify({
         "status": "ok",
         "timestamp": datetime.datetime.utcnow().isoformat()
     })
 
+
 @app.route("/")
 def hello():
     return jsonify({
         "message": "Hello from my DevOps app!"
     })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
